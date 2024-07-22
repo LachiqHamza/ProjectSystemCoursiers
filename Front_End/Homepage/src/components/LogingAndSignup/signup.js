b26320c9171bb5a221a69551e9d06b0a9cbdc64b
HamzaLachiq <hamzalachiq2@gmail.com>
2024-07-22 13:48:16 +0100

    signup done

--- Front_End/Homepage/src/components/LogingAndSignup/signup.js
+++ Front_End/Homepage/src/components/LogingAndSignup/signup.js
@@ -1,63 +1,66 @@
 import React, { useState } from 'react';
-import {
-  AutoComplete,
-  Button,
-  Cascader,
-  Checkbox,
-  Col,
-  Form,
-  Input,
-  Row,
-  Select,
-} from 'antd';
+import { AutoComplete, Button, Cascader, Checkbox, Col, Form, Input, Row, Select, notification } from 'antd';
 import { residences } from './residences';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './SignUp.css';
+import axios from 'axios';
 
 const { Option } = Select;
 const formItemLayout = {
   labelCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 8,
-    },
+    xs: { span: 24 },
+    sm: { span: 8 },
   },
   wrapperCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 16,
-    },
+    xs: { span: 24 },
+    sm: { span: 16 },
   },
 };
 const tailFormItemLayout = {
   wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
+    xs: { span: 24, offset: 0 },
+    sm: { span: 16, offset: 8 },
   },
 };
+
 const SignUp = () => {
   const [form] = Form.useForm();
-  const onFinish = (values) => {
+  const navigate = useNavigate();
+
+  const onFinish = async (values) => {
     console.log('Received values of form: ', values);
+    try {
+      const response = await axios.post('http://localhost:8000/api/clients/add', {
+        name: values.name,
+        lastname: values.lastname,
+        email: values.email,
+        password: values.password,
+        tele: values.phone,
+        role: 'client'
+      });
+      console.log('Server response:', response);
+
+      notification.success({
+        message: 'Registration Successful',
+        description: 'You have successfully registered. Redirecting to login page...',
+      });
+
+      setTimeout(() => {
+        navigate('/login');
+      }, 2000); // Redirect after 2 seconds
+    } catch (error) {
+      console.error('There was an error submitting the form!', error);
+      notification.error({
+        message: 'Registration Failed',
+        description: 'There was an issue with your registration. Please try again.',
+      });
+    }
   };
+
   const prefixSelector = (
     <Form.Item name="prefix" noStyle>
-      <Select
-        style={{
-          width: 70,
-        }}
-      >
-        <Option value="86">+212</Option>
+      <Select style={{ width: 70 }}>
+        <Option value="212">+212</Option>
       </Select>
     </Form.Item>
   );
@@ -74,6 +77,7 @@ const SignUp = () => {
     label: website,
     value: website,
   }));
+
   return (
     <div className="center-container">
       <Form
@@ -81,27 +85,16 @@ const SignUp = () => {
         form={form}
         name="register"
         onFinish={onFinish}
-        initialValues={{
-          residence: ['CasaBlanca', 'Rabat', 'MARRAKECH', '...'],
-          prefix: '86',
-        }}
-        style={{
-          maxWidth: 600,
-        }}
+        initialValues={{ residence: ['CasaBlanca', 'Rabat', 'MARRAKECH', '...'], prefix: '212' }}
+        style={{ maxWidth: 600 }}
         scrollToFirstError
       >
         <Form.Item
           name="email"
           label="E-mail"
           rules={[
-            {
-              type: 'email',
-              message: 'The input is not valid E-mail!',
-            },
-            {
-              required: true,
-              message: 'Please input your E-mail!',
-            },
+            { type: 'email', message: 'The input is not valid E-mail!' },
+            { required: true, message: 'Please input your E-mail!' },
           ]}
         >
           <Input />
@@ -110,12 +103,7 @@ const SignUp = () => {
         <Form.Item
           name="password"
           label="Password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
+          rules={[{ required: true, message: 'Please input your password!' }]}
           hasFeedback
         >
           <Input.Password />
@@ -127,16 +115,13 @@ const SignUp = () => {
           dependencies={['password']}
           hasFeedback
           rules={[
-            {
-              required: true,
-              message: 'Please confirm your password!',
-            },
+            { required: true, message: 'Please confirm your password!' },
             ({ getFieldValue }) => ({
               validator(_, value) {
                 if (!value || getFieldValue('password') === value) {
                   return Promise.resolve();
                 }
-                return Promise.reject(new Error('The new password that you entered do not match!'));
+                return Promise.reject(new Error('The passwords do not match!'));
               },
             }),
           ]}
@@ -145,16 +130,17 @@ const SignUp = () => {
         </Form.Item>
 
         <Form.Item
-          name="nickname"
-          label="Nickname"
-          tooltip="What do you want others to call you?"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your nickname!',
-              whitespace: true,
-            },
-          ]}
+          name="name"
+          label="First Name"
+          rules={[{ required: true, message: 'Please input your first name!', whitespace: true }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="lastname"
+          label="Last Name"
+          rules={[{ required: true, message: 'Please input your last name!', whitespace: true }]}
         >
           <Input />
         </Form.Item>
@@ -162,13 +148,7 @@ const SignUp = () => {
         <Form.Item
           name="residence"
           label="Habitual Residence"
-          rules={[
-            {
-              type: 'array',
-              required: true,
-              message: 'Please select your habitual residence!',
-            },
-          ]}
+          rules={[{ type: 'array', required: true, message: 'Please select your habitual residence!' }]}
         >
           <Cascader options={residences} />
         </Form.Item>
@@ -176,29 +156,16 @@ const SignUp = () => {
         <Form.Item
           name="phone"
           label="Phone Number"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your phone number!',
-            },
-          ]}
+          rules={[{ required: true, message: 'Please input your phone number!' }]}
         >
-          <Input
-            addonBefore={prefixSelector}
-            style={{
-              width: '100%',
-            }}
-          />
+          <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
         </Form.Item>
 
         <Form.Item
           name="agreement"
           valuePropName="checked"
           rules={[
-            {
-              validator: (_, value) =>
-                value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-            },
+            { validator: (_, value) => value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')) },
           ]}
           {...tailFormItemLayout}
         >
@@ -207,9 +174,7 @@ const SignUp = () => {
           </Checkbox>
         </Form.Item>
         <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
+          <Button type="primary" htmlType="submit">Register</Button>
           <Button type="primary" htmlType="button" style={{ marginLeft: '10px' }}>
             <Link to="/login" style={{ color: 'inherit' }}>Go Back to LogIn</Link>
           </Button>
