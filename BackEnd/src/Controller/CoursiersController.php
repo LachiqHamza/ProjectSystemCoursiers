5c9affea1c939f098669de98a6f1ceb92deb59f0
chmode <salaheddinepy@gmail.com>
2024-07-25 12:16:57 +0100

    fix add coursier v2

--- BackEnd/src/Controller/CoursiersController.php
+++ BackEnd/src/Controller/CoursiersController.php
@@ -103,7 +103,7 @@ class CoursiersController extends AbstractController
         $tele = $data['tele'] ?? null;
         $email = $data['email'] ?? null;
         $password = $data['password'] ?? null;
-        $role = $data['role'] ?? "coursier";
+        //$role = $data['role'] ?? "coursier";
         $cin = $data['Cin'] ?? null;
         $dateIntegration = isset($data['Date_intergration']) ? new \DateTime($data['Date_intergration']) : null;
         $salaire = isset($data['salaire']) ? floatval($data['salaire']) : null;
@@ -128,7 +128,7 @@ class CoursiersController extends AbstractController
 
 
         // Create a new courcier
-        $courcier = new Coursiers($nom, $prenom, $tele, $email, $password, $role, $cin, $dateIntegration, $salaire);
+        $courcier = new Coursiers($nom, $prenom, $tele, $email, $password, "coursier", $cin, $dateIntegration, $salaire);
 
         // Persist the new courcier to the database
         $entityManager->persist($courcier);
